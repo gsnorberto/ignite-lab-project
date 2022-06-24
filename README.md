25601beb90028c85de1bd69f8ad038375729a904
“Gabriel <sn_gabriel@outlook.com>
2022-06-24 15:03:12 -0300

    Initial configs

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 React
 
 npm create vite@latest
+npm i tailwindcss postcss autoprefixer -D
+npx tailwindcss init -p
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [ //files that will use the tailwind
+        './src/**/*.tsx' //all files with ".tsx"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
